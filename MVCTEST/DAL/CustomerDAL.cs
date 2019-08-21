6ee1dbe6e4fe147f767af21ea01612a3f3e6a5fd
dipak <dip@3sixtyit.com>
2019-08-21 14:16:59 +0530

    Commit Script and code First eF added

---
+++ MVCTEST/DAL/CustomerDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MVCTEST.Models;
+namespace MVCTEST.DAL
+{
+    public class CustomerDAL : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Customer>().ToTable("tblCustomer");
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
