f44741a7079916ab93ef169af17bfac79395dcbe
dipak <dip@3sixtyit.com>
2019-08-21 09:27:40 +0530

    Commit

---
+++ MVCTEST/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTEST.Models;
+
+namespace MVCTEST.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Customer()
+        {
+            Customer obj = new Customer  { CustomerCode ="dip1001",CustomerName ="Dipak" };
+            return View(obj);
+        }
+    }
+}
