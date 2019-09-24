4f92268b660e25ba528a17a0cf232fdb95074eb1
dipak <dip@3sixtyit.com>
2019-09-24 10:47:46 +0530

    Commit

---
+++ MVCTEST/Models/Extended/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCTEST.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+
+    }
+
+    public class UserMetadata
+    {
+        [Required(AllowEmptyStrings =false,ErrorMessage ="First Name is Required")]
+        [Display(Name ="First Name1")]
+        public string FirstName { get; set; }
+    }
+}
